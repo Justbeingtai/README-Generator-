f144dd6ed8da9c6489ab769d1bf8f3b7f387a725
Tai Ho <taiho051493@yahoo.com>
2024-08-11 23:16:37 -0500

    completed everything now just need to make a new readme

--- README.md
+++ README.md
@@ -1,11 +1,10 @@
-function generateReadme(answers) {
-  return `
-# ${answers.title}
 
-![License](https://img.shields.io/badge/License-${answers.license}-blue.svg)
+# Readme Generator
+
+![License](https://img.shields.io/badge/License-MIT-blue.svg)
 
 ## Description
-${answers.description}
+Generate a new readme after answering questions
 
 ## Table of Contents
 - [Installation](#installation)
@@ -15,23 +14,33 @@ ${answers.description}
 - [Questions](#questions)
 
 ## Installation
-${answers.installation}
+1. Initialize the project by creating a basic package.json file:
+   ```bash
+   npm init
+   ```
+    2. Install the necessary dependencies:
+   ```bash
+   npm install inquirer
+   ```
 
 ## Usage
-${answers.usage}
+1. Require the `inquirer` package in your JavaScript file:
+   ```javascript
+   const inquirer = require('inquirer');
+   ```
+    2. Use `inquirer` to create a list of prompts in the terminal that ask questions and display the answers in a newly generated README file.
 
 ## Contributing
-${answers.contributing}
+I've utilized resources from my bootcamp classes, Stack Overflow, and WEBmd to help me complete this project. Contributions are welcome from anyone, but please make sure to follow the existing code style and submit any changes via pull requests.
 
 ## Tests
-${answers.tests}
+I've run the tests through the terminal and observed the successful generation of a new README file. To test, simply run the application and verify that the README.md file is created as expected.
 
 ## License
-This project is licensed under the ${answers.license} license.
+This project is licensed under the MIT license.
 
 ## Questions
 For any questions, please contact me:
-- GitHub: [${answers.github}](https://github.com/${answers.github})
-- Email: ${answers.email}
-`;
-}
+- GitHub: [justbeingtai](https://github.com/justbeingtai)
+- Email: tai@gmail.com
+  
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 inquirer 
-  .promt ( [
+  .prompt ( [
   {
     type: 'input',
     name: 'title',
@@ -12,6 +12,41 @@ inquirer
     type: 'input',
     name: 'description',
     message: 'Provide a description of your project:',
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'Provide installation instructions:',
+    default: `1. Initialize the project by creating a basic package.json file:
+   \`\`\`bash
+   npm init
+   \`\`\`
+    2. Install the necessary dependencies:
+   \`\`\`bash
+   npm install inquirer
+   \`\`\``
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'Provide usage information:',
+    default: `1. Require the \`inquirer\` package in your JavaScript file:
+   \`\`\`javascript
+   const inquirer = require('inquirer');
+   \`\`\`
+    2. Use \`inquirer\` to create a list of prompts in the terminal that ask questions and display the answers in a newly generated README file.`
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'Provide contribution guidelines:',
+    default: `I've utilized resources from my bootcamp classes, Stack Overflow, and WEBmd to help me complete this project. Contributions are welcome from anyone, but please make sure to follow the existing code style and submit any changes via pull requests.`
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'Provide test instructions:',
+    default: `I've run the tests through the terminal and observed the successful generation of a new README file. To test, simply run the application and verify that the README.md file is created as expected.`
   },
   {
     type: 'list',
@@ -36,3 +71,41 @@ inquirer
     err ? console.log(err) : console.log('README.md generated successfully!')
   );
 });
+
+function generateReadme(answers) {
+  return `
+# ${answers.title}
+
+![License](https://img.shields.io/badge/License-${answers.license}-blue.svg)
+
+## Description
+${answers.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+${answers.installation}
+
+## Usage
+${answers.usage}
+
+## Contributing
+${answers.contributing}
+
+## Tests
+${answers.tests}
+
+## License
+This project is licensed under the ${answers.license} license.
+
+## Questions
+For any questions, please contact me:
+- GitHub: [${answers.github}](https://github.com/${answers.github})
+- Email: ${answers.email}
+  `;
+}
