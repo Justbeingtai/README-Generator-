f7e808289c0a015e522ce5ecc08a86a7a4cc81aa
Tai Ho <taiho051493@yahoo.com>
2024-08-11 22:59:59 -0500

    fixed require and working on readme file

--- README.md
+++ README.md
@@ -1,3 +1,37 @@
-# Professional README Generator Starter Code
+function generateReadme(answers) {
+  return `
+# ${answers.title}
 
-[How to create a Professional README](https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide)
+![License](https://img.shields.io/badge/License-${answers.license}-blue.svg)
+
+## Description
+${answers.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+${answers.installation}
+
+## Usage
+${answers.usage}
+
+## Contributing
+${answers.contributing}
+
+## Tests
+${answers.tests}
+
+## License
+This project is licensed under the ${answers.license} license.
+
+## Questions
+For any questions, please contact me:
+- GitHub: [${answers.github}](https://github.com/${answers.github})
+- Email: ${answers.email}
+`;
+}
--- index.js
+++ index.js
@@ -1,3 +1,38 @@
-const inquirer = request('inquirer');
-const fs = request('fs');
+const inquirer = require('inquirer');
+const fs = require('fs');
 
+inquirer 
+  .promt ( [
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Provide a description of your project:',
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'Choose a license for your project:',
+    choices: ['MIT', 'GPLv3', 'Apache 2.0', 'None'],
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: 'Enter your GitHub username:',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'Enter your email address:',
+  }
+])
+  .then((answers) => {
+  const readmeContent = generateReadme(answers);
+  fs.writeFile('README.md', readmeContent, (err) =>
+    err ? console.log(err) : console.log('README.md generated successfully!')
+  );
+});
